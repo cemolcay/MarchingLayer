38e08f18cbb2c86882f6ba9165836328b26843bd
cemolcay <ccemolcay@gmail.com>
2017-11-12 19:04:00 +0300

    Basic animation setup

--- MarchingLayer/MarchingLayer.swift
+++ MarchingLayer/MarchingLayer.swift
@@ -24,20 +24,56 @@ public enum MarchingLayerAnimationDirection {
 public class MarchingSpriteLayer: CALayer {
   /// Image of the sprite layer renders.
   var sprite: UIImage?
+  /// Size of the sprite.
+  var size: CGSize
+  /// Set tint color of the sprites if you use template images.
+  var tintColor: UIColor?
+
+  /// Initilize with a sprite image and optional preferred sprite size.
+  ///
+  /// - Parameters:
+  ///   - sprite: Image reference of sprite.
+  ///   - preferredSize: Optional preferred size for sprite. Defaults nil. Leave nil for using original image size.
+  init(sprite: UIImage, preferredSize: CGSize? = nil, tintColor: UIColor? = nil) {
+    self.size = preferredSize ?? sprite.size
+    self.sprite = sprite
+    self.tintColor = tintColor
+    super.init()
+    // Setup sprite
+    frame = CGRect(origin: .zero, size: self.size)
+    contentsGravity = kCAGravityResizeAspect
+    contents = sprite.cgImage
+  }
+
+  public override init(layer: Any) {
+    self.size = .zero
+    super.init(layer: layer)
+  }
+
+  public required init?(coder aDecoder: NSCoder) {
+    self.size = .zero
+    super.init(coder: aDecoder)
+  }
 }
 
 /// Randomly fills layer with sprites and move them in any direction and speed you want.
 public class MarchingLayer: CALayer {
   /// Marching sprites. Layer randomly pops images from this array.
-  public var sprites = [UIImage]()
+  public var sprites = [UIImage]() { didSet{ setupSprites() }}
   /// Direction of the marching animation.
   public var animationDirection = MarchingLayerAnimationDirection.up
   /// Speed of the marching animation.
-  public var animationSpeed: CGFloat = 5.0
+  public var animationSpeed: CGFloat = 0.1
   /// Horizontal spacing between sprites.
   public var horizontalSpriteSpacing: CGFloat = 10.0
   /// Vertical spacing between sprites.
   public var verticalSpriteSpacing: CGFloat = 10.0
+  /// Set if you want to make all sprites size equal.
+  /// Leave it nil if you want to use original sprite sizes. Defaults nil.
+  public var preferredSpriteSize: CGSize?
+  /// Set if you want to make all sprites same tint color. Leave it nil if you want to use original images.
+  /// Setup images as template images if you want to use that feature. Defaults nil.
+  public var preferredSpriteTintColor: UIColor?
   /// Read-only property to check if layer is animating.
   public private(set) var isAnimating = false
   /// All sprite sublayers in layer.
@@ -87,7 +123,39 @@ public class MarchingLayer: CALayer {
 
   /// Pops the initial sprites into empty layer.
   private func setupSprites() {
+    // Reset sublayers
+    marchingSprites.forEach({ $0.removeFromSuperlayer() })
+    marchingSprites = []
 
+    var currentX: CGFloat = 0
+    var currentY: CGFloat = 0
+    var maxY: CGFloat = 0
+    while currentY < frame.size.height {
+      guard let randomImage = randomImage else { continue }
+      let randomSprite = MarchingSpriteLayer(
+        sprite: randomImage,
+        preferredSize: preferredSpriteSize,
+        tintColor: preferredSpriteTintColor)
+      marchingSprites.append(randomSprite)
+      addSublayer(randomSprite)
+
+      // Layout sprite.
+      randomSprite.frame.origin = CGPoint(x: currentX, y: currentY)
+      currentX += randomSprite.size.width + horizontalSpriteSpacing
+      maxY = max(maxY, randomSprite.size.height)
+
+      // Move to new row.
+      if currentX > frame.size.width {
+        currentX = 0
+        currentY += maxY + verticalSpriteSpacing
+        maxY = 0
+      }
+
+      // End loop if sprites laid out.
+      if currentY > frame.size.height {
+        break
+      }
+    }
   }
 
   // MARK: Animating
@@ -96,7 +164,7 @@ public class MarchingLayer: CALayer {
   public func startAnimation() {
     isAnimating = true
     marchingAnimationTimer = Timer.scheduledTimer(
-      timeInterval: TimeInterval(animationSpeed),
+      timeInterval: 1.0/60.0, // 60 FPS
       target: self,
       selector: #selector(marchingAnimation),
       userInfo: nil,
@@ -112,18 +180,37 @@ public class MarchingLayer: CALayer {
 
   /// Marhing animation tick.
   @objc private func marchingAnimation() {
+    var removingSprites = [MarchingSpriteLayer]()
     for sprite in marchingSprites {
+      var shouldRemove = false
       switch animationDirection {
       case .up:
         sprite.frame.origin.y -= animationSpeed
-        break
+        shouldRemove = sprite.frame.origin.y + sprite.frame.size.height + (sprite.frame.size.height / 2) < 0
       case .down:
-        break
+        sprite.frame.origin.y += animationSpeed
+        shouldRemove = sprite.frame.origin.y - (sprite.frame.size.height / 2) > frame.size.height
       case .left:
-        break
+        sprite.frame.origin.x -= animationSpeed
+        shouldRemove = sprite.frame.origin.x + sprite.frame.size.width + (sprite.frame.size.width / 2)  < 0
       case .right:
-        break
+        sprite.frame.origin.x += animationSpeed
+        shouldRemove = sprite.frame.origin.x - sprite.frame.size.width > frame.size.width
+      }
+
+      if shouldRemove {
+        removingSprites.append(sprite)
       }
     }
+
+    for sprite in removingSprites {
+      guard let index = marchingSprites.index(of: sprite) else { continue }
+      CATransaction.begin()
+      CATransaction.setValue(kCFBooleanTrue, forKey: kCATransactionDisableActions)
+      marchingSprites.remove(at: index).removeFromSuperlayer()
+      CATransaction.commit()
+    }
+
+    removingSprites.removeAll()
   }
 }
--- MarchingLayer/ViewController.swift
+++ MarchingLayer/ViewController.swift
@@ -13,10 +13,13 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    marchingLayer.frame = view.frame//.offsetBy(dx: 20, dy: 20)
-//    marchingLayer.borderColor = UIColor.red.cgColor
-//    marchingLayer.borderWidth = 1
+    marchingLayer.frame = view.frame.insetBy(dx: 100, dy: 150)//.offsetBy(dx: 20, dy: 20)
+    marchingLayer.borderColor = UIColor.red.cgColor
+    marchingLayer.borderWidth = 1
     view.layer.addSublayer(marchingLayer)
+    marchingLayer.animationDirection = .right // down up left
+    marchingLayer.animationSpeed = 1
     marchingLayer.sprites = [Int](1...15).flatMap({ UIImage(named: "musicSheetSliced\($0)") })
+    marchingLayer.startAnimation()
   }
 }
